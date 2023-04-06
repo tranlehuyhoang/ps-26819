0c344f1aa595c8646f84a18c0812b0c6f92994e8
Huyh Huyhu <2509roblox@gmail.com>
2023-04-06 12:08:52 +0700

    afaew

---
+++ javascript/main.js
@@ -0,0 +1,12 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCQ1i7ayjCOy4lnsUwsHWQAORs02YoQntQ",
+    authDomain: "fir-3b1e0.firebaseapp.com",
+    projectId: "fir-3b1e0",
+    storageBucket: "fir-3b1e0.appspot.com",
+    messagingSenderId: "457136927764",
+    appId: "1:457136927764:web:b17b0120db7091651f2301",
+    measurementId: "G-8LW6KBNNJ5"
+};
+// Khởi tạo Firebase
+firebase.initializeApp(firebaseConfig);
